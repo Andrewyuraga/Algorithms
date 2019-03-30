c5c7c2ef03fbeaaac0b1fe6b44f4921d0e1ed426
Andrei <mr.yuraga@gmail.com>
2019-03-30 19:02:42 +0300

    Test git

---
+++ algorithms/src/binaryseach/test.java
@@ -0,0 +1,16 @@
+package binaryseach;
+
+/**
+ * Created by AYuraga on 2019-03-30.
+ */
+public class test {
+    public static void main(String[] args) {
+        pringArguments(args);
+    }
+
+    private static void pringArguments(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
